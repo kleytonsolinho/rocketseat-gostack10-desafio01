218bb09b38dad52b548b0520198e548591ffd904
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-01-29 19:11:02 -0300

    pre POST

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const server = express();
-//server.use(express.json());
+server.use(express.json());
 
-const projects = ["ID", "Projeto", "Task"];
+const projects = [];
 
-server.get('/projects', (req, res) => {
+server.use((req, res, next) => {
+  console.time('Time Request');
+  console.log(`Last method: ${req.method}; Last URL: ${req.url}`)
+
+  next();
+
+  console.timeEnd('Time Request');
+});
+
+function CheckProject(req, res, next) {
+  if (!req.projects) {
+    return res.status(400).json({ error: 'Projects does not exist!' });
+  }
+
+  return next();
+}
+
+server.get('/projects', CheckProject, (req, res) => {
   //console.log("Funcionando");
 
   return res.json(projects);
