3480a92c5eed6850235af69335cd222f2d812a7d
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-01-30 18:13:07 -0300

    POST and GET CONFIG

--- index.js
+++ index.js
@@ -13,18 +13,44 @@ server.use((req, res, next) => {
   console.timeEnd('Time Request');
 });
 
-function CheckProject(req, res, next) {
-  if (!req.projects) {
+function CheckProjectList(req, res, next) {
+  if (!req.params.projects) {
+    return res.status(400).json({ error: 'Projects list is empty!' });
+  }
+
+  return next();
+}
+
+function CheckProjectInArray(req, res, next) {
+  if (!projects[req.params.index]) {
     return res.status(400).json({ error: 'Projects does not exist!' });
   }
 
   return next();
 }
 
-server.get('/projects', CheckProject, (req, res) => {
+server.get('/projects', (req, res) => {
   //console.log("Funcionando");
 
   return res.json(projects);
 })
+
+server.get('/projects/:index', CheckProjectInArray, (req, res) => {
+  const { index } = req.params;
+
+  return res.json(projects[index]);
+})
+
+server.post('/projects', (req, res) => {
+  const { id } = req.body;
+  const { title } = req.body;
+  const { task } = req.body;
+
+  projects.push(id);
+  projects.push(title);
+  projects.push(task);
+
+  return res.json(projects);
+});
 
 server.listen(3000);
